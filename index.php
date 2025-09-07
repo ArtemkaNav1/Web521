649880bd6692ccde7ae1b707fd2466c3337ba069
ArtemkaNav1 <artem.navicki@gmail.com>
2025-09-07 19:30:46 +0500

    lesson two

--- index.php
+++ index.php
@@ -8,27 +8,23 @@
 </head>
 
 <body>
-    <h3> Задача: У вас есть массив чисел. Используйте метод map, чтобы
-        создать новый массив, в котором каждое число умножено на 2.
-        ● Пример: [1, 2, 3, 4] -> [2, 4, 6, 8]
+    <h3>
+        Использование filter:
+        ● Задача: У вас есть массив строк. Используйте метод filter, чтобы
+        отфильтровать строки, длина которых больше 5 символов.
+        ● Пример: ['apple', 'banana', 'kiwi', 'grapefruit'] -> ['banana',
+        'grapefruit']
     </h3>
 
     <?php
-    $numbers = [
-        1 => 1,
-        2 => 2,
-        3 => 3,
-        4 => 4,
-    ];
+    $words = ['apple', 'banana', 'kiwi', 'grapefruit'];
 
+    // filter
+    $filtered = array_filter($words, fn($word) => strlen($word) > 5);
 
-
-    $doubled = array_map(fn($num) => $num * 2, $numbers);
-
-
-    print_r($doubled);
+    print_r(array_values($filtered)); // сброс индексов
     ?>
-    <script src="script.js"></script>
+
 </body>
 
 </html>
