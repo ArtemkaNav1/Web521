5e46e38e6c2fe75c9c2e38778bd8ff5180972e31
ArtemkaNav1 <artem.navicki@gmail.com>
2025-09-07 19:41:16 +0500

    Add lesson three

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <h3>
+      Использование reduce: ● Задача: У вас есть массив чисел. Используйте метод
+      reduce, чтобы найти сумму всех чисел в массиве. ● Пример: [1, 2, 3, 4] ->
+      10
+    </h3>
+
+    
+  </body>
+</html>
