879957dcd41e657e8e296e4cb73cbc5aa0fc81e2
ArtemkaNav1 <artem.navicki@gmail.com>
2025-10-19 22:27:02 +0500

    Модуль 1, Массивы. Часть 2. Задание 2

--- index.php
+++ index.php
@@ -11,11 +11,14 @@
 </head>
 
 <body>
-    <h1>Задание 1</h1>
+  
   <h2>Сотрудники компаний: </h2>
 
     <?php
-    // Массив из 10 элементов
+
+    // Задание 1:
+
+    echo "<h2>Задание 1</h2>";
     $employees = [
         ['name' => 'Serhii', 'company' => 'Microsoft', 'position' => 'Developer'],
         ['name' => 'Oleh', 'company' => 'Apple', 'position' => 'QC Engineer'],
@@ -34,7 +37,55 @@
         echo "<p>{$employee['name']}</strong> is working in {$employee['company']} on position: {$employee['position']}</p>";
     }
     echo "<hr>";
+
+
+    // Задание 2:
+
+    echo "<h2>Задание 2</h2>";
+
+     $employees = [
+        ['name' => 'Serhii', 'company' => 'Microsoft', 'position' => 'Developer'],
+        ['name' => 'Oleh', 'company' => 'Apple', 'position' => 'QC Engineer'],
+        ['name' => 'Olya', 'company' => 'Amazon', 'position' => 'Project Manager'],
+        ['name' => 'Oksana', 'company' => 'ITStep', 'position' => 'Teacher'],
+        ['name' => 'Andrii', 'company' => 'Apple', 'position' => 'Solution Architect'],
+        ['name' => 'Ihor', 'company' => 'Samsung', 'position' => 'Engineer'],
+        ['name' => 'Olesya', 'company' => 'Samsung', 'position' => 'Developer'],
+        ['name' => 'Roman', 'company' => 'Microsoft', 'position' => 'Product Owner'],
+        ['name' => 'Yura', 'company' => 'Microsoft', 'position' => 'QC Engineer'],
+        ['name' => 'Iryna', 'company' => 'ITStep', 'position' => 'Teacher']
+    ];
+
+    // Группируем сотрудников по компаниям
+    $companies = [];
+    foreach ($employees as $employee) {
+        $companyName = $employee['company'];
+        if (!isset($companies[$companyName])) {
+            $companies[$companyName] = [];
+        }
+        $companies[$companyName][] = $employee['name'];
+    }
+
+    // Выводим сгруппированный список
+    echo "<ul>";
+    foreach ($companies as $company => $employeesList) {
+        echo "<li><strong>$company</strong>";
+        echo "<ul>";
+        foreach ($employeesList as $employeeName) {
+            echo "<li>$employeeName</li>";
+        }
+        echo "</ul>";
+        echo "</li>";
+    }
+    echo "</ul>";
+
+    echo "<hr>";
+
+
     ?>
+    
+    
+   
 </body>
 
 </html>
