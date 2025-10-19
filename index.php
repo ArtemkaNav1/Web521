0d9c58a170a7f09a2aa813396f472fc21c90a520
ArtemkaNav1 <artem.navicki@gmail.com>
2025-10-19 21:52:10 +0500

    Модуль 1, Массивы. Часть 2. Задание 1

--- index.php
+++ index.php
@@ -5,69 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+    
+    </style>
 </head>
 
 <body>
-    <h3>
-        1. Конкатенация: вывод на страницу («Hello! My name
-        is 'Name'»), где «Name» — это переменная, в которую
-        вводится имя (выводится на странице в кавычках).
-        <br>
-        2. Добавить к заданию 1 фразу «I’m Age», где Age — это
-        переменная с возрастом студента (выводится с новой
-        строки).
-        <br>
-        3. В задание 3 из практических добавить вывод действий в таком формате: 'a'+'b'='rez', где a — это значение
-        1-й переменной, b — это значение 2-й переменной,
-        rez — результат операции между ними
-        <br>
-        4. Поменять 2 числа местами без использования 3-й переменной
-        <br>
-        5. Разработать php-страницу, в которой выбрать правильные html-элементы и вывести 3 вопроса в следующем формате:
-        a) 1 — вопрос с 4 вариантами ответа и только 1 из
-        них правильный;
-        b) 2 — вопрос с 4 вариантами ответа и может быть
-        несколько правильных;
-        c) 3 — вопрос с развернутым ответом
-    </h3>
+    <h1>Задание 1</h1>
+  <h2>Сотрудники компаний: </h2>
 
     <?php
-    echo "<tr><td><b>Задание 1:</b><br>:";
-    $name = "Артём"; //  Имя
-    
-    echo "Hello! My name is '" . $name . "'" . "<br>";
-
-    echo "<tr><td><b>Задание 2:</b><br>:";
-    $name = "Артём";
-    $age = 32;
-
-    echo "Hello! My name is '" . $name . "'" . "<br>";
-    echo "I'm $age" . "<br>";
-
-    echo "<tr><td><b>Задание 3:</b><br>";
-    $a = 10;
-    $b = 5;
-
-    //Операции
-    echo "'$a' + '$b' = '" . ($a + $b) . "'<br>";
-    echo "'$a' - '$b' = '" . ($a - $b) . "'<br>";
-    echo "'$a' * '$b' = '" . ($a * $b) . "'<br>";
-    echo "'$a' / '$b' = '" . ($a / $b) . "'<br>";
-
-    echo "<tr><td><b>Задание 4:</b><br>";
-
-    $a = 2;
-    $b = 10;
-    echo "Было: первое число = $a, второе число = $b <br>";
-    $a = $a + $b;
-    $b = $a - $b;
-    $a = $a - $b;
-    echo "Стало: первое число = $a, второе число = $b<br/>";
-
+    // Массив из 10 элементов
+    $employees = [
+        ['name' => 'Serhii', 'company' => 'Microsoft', 'position' => 'Developer'],
+        ['name' => 'Oleh', 'company' => 'Apple', 'position' => 'QC Engineer'],
+        ['name' => 'Olya', 'company' => 'Amazon', 'position' => 'Project Manager'],
+        ['name' => 'Oksana', 'company' => 'ITStep', 'position' => 'Teacher'],
+        ['name' => 'Andrii', 'company' => 'Apple', 'position' => 'Solution Architect'],
+        ['name' => 'Ihor', 'company' => 'Samsung', 'position' => 'Engineer'],
+        ['name' => 'Olesya', 'company' => 'Samsung', 'position' => 'Developer'],
+        ['name' => 'Roman', 'company' => 'Microsoft', 'position' => 'Product Owner'],
+        ['name' => 'Yura', 'company' => 'Microsoft', 'position' => 'QC Engineer'],
+        ['name' => 'Iryna', 'company' => 'ITStep', 'position' => 'Teacher']
+    ];
 
     
-
-
+    foreach ($employees as $employee) {
+        echo "<p>{$employee['name']}</strong> is working in {$employee['company']} on position: {$employee['position']}</p>";
+    }
+    echo "<hr>";
     ?>
 </body>
 
